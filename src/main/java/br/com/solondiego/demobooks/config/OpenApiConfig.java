acccac95acf261dba8beb724d7a7b8b7248731a4
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2024-06-05 21:48:00 -0300

    update

---
+++ src/main/java/br/com/solondiego/demobooks/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package br.com.solondiego.demobooks.config;
+
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+public class OpenApiConfig {
+	
+	@Bean
+	OpenAPI customOpenAPI() {
+		
+	}
+
+}
